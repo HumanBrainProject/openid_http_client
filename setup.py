65592004a86012d92185aae61b43ae3e3d0512dc
David Kunzmann <david.kunzmann@epfl.ch>
2018-03-13 15:04:51 +0100

    Added user name in pypirc

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = parse_requirements("http_client/requirements.txt", session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='http_client',
+    name='pyxus_http_client',
     version='0.0.1',
     packages=['http_client', 'http_client.auth_client', 'http_client.test'],
     install_requires = reqs,
@@ -14,6 +14,7 @@ setup(
     author = 'HPB Team',
     author_email = 'platform@humanbrainproject.eu',
     keywords = ['pyxus', 'http', 'client'],
-    classifiers = []
-
+    classifiers = [],
+    url = 'https://gitlab.humanbrainproject.org/HumanBrainProject/pyxus_http_client',
+    download_url = 'https://gitlab.humanbrainproject.org/HumanBrainProject/pyxus_http_client/repository/master/archive.zip'
 )
