b521b53fab7dd5b1a4ec8a11f93ef233adeec4c4
David Kunzmann <david.kunzmann@epfl.ch>
2018-03-14 10:51:45 +0100

    Corrected copyright owner

--- pyxus_http_client/pyxus_http_client/auth_client/auth_client.py
+++ pyxus_http_client/pyxus_http_client/auth_client/auth_client.py
@@ -1,4 +1,4 @@
-#   Copyright 2018 HBP Team
+#   Copyright 2018 HumanBrainProject
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
