66e7a4b299ac54abedd62458114bed6e3a35ae79
Oliver Schmid <oliver.schmid@epfl.ch>
2018-06-21 08:56:33 +0200

    Streamline logging

--- openid_http_client/setup.py
+++ openid_http_client/setup.py
@@ -26,7 +26,7 @@ else:
 
 setup(
     name='openid_http_client',
-    version='0.0.19',
+    version='0.0.20',
     packages=['openid_http_client', 'openid_http_client.auth_client'],
     install_requires = ["requests", "curlify"],
     scripts=['manage.py'],
